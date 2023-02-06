bc0a8433f3b5b8ab101cdfbc4375c2e7728b49f6
dina-salah <dinasalah35586@gmail.com>
2023-02-06 23:57:19 +0200

    trying to add routing in the header comp.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SginupComponent } from './sginup/sginup.component';
+import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
-  {path: 'sginup', component: SginupComponent},
-  {path:'home', component: HomeComponent}
+  {path: 'sign-in', component: SigninComponent},
+  {path: 'sign-up', component: SginupComponent},
+  {path: 'home', component:HomeComponent}
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import {Router , RouterModule} from '@angular/router'
+import { RouterModule} from '@angular/router'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
